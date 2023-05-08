b33ba74245f83a245f4511c6cf11dcdd801252fc
Natalia328 <nswierczek@gmail.com>
2023-05-08 14:54:02 +0200

    animation update

--- dev/js/project.js
+++ dev/js/project.js
@@ -60,58 +60,98 @@ window.addEventListener("scroll", contactTitleEffect)
 // var word2 = document.getElementsByClassName('text-3');
 // var word3 = document.getElementsByClassName('text-4');
 
-// var words = [word1, word2, word3];
-var words = document.getElementsByClassName('aboutMeText');
+
+// var words = document.getElementsByClassName('aboutMeText');
+// var wordArray = [];
+// var currentWord = 0;
+
+// words[currentWord].style.opacity = 1;
+// for (var i = 0; i < words.length; i++) {
+//   splitLetters(words[i]);
+// }
+
+// function changeWord() {
+//   var cw = wordArray[currentWord];
+//   var nw = currentWord == words.length-1 ? wordArray[0] : wordArray[currentWord+1];
+//   for (var i = 0; i < cw.length; i++) {
+//     animateLetterOut(cw, i);
+//   }
+  
+//   for (var i = 0; i < nw.length; i++) {
+//     nw[i].className = 'letter behind';
+//     nw[0].parentElement.style.opacity = 1;
+//     animateLetterIn(nw, i);
+//   }
+  
+//   currentWord = (currentWord == wordArray.length-1) ? 0 : currentWord+1;
+// }
+
+// function animateLetterOut(cw, i) {
+//   setTimeout(function() {
+// 		cw[i].className = 'letter out';
+//   }, i*80);
+// }
+
+// function animateLetterIn(nw, i) {
+//   setTimeout(function() {
+// 		nw[i].className = 'letter in';
+//   }, 340+(i*80));
+// }
+
+// function splitLetters(word) {
+//   var content = word.innerHTML;
+//   word.innerHTML = '';
+//   var letters = [];
+//   for (var i = 0; i < content.length; i++) {
+//     var letter = document.createElement('span');
+//     letter.className = 'letter';
+//     letter.innerHTML = content.charAt(i);
+//     word.appendChild(letter);
+//     letters.push(letter);
+//   }
+  
+//   wordArray.push(letters);
+// }
+
+// changeWord();
+// setInterval(changeWord, 2000);
+
+
+// Wrap every letter in a span
+var textWrapper = document.querySelectorAll('.aboutMeText');
 var wordArray = [];
-var currentWord = 0;
+for (var i = 0; i < textWrapper.length; i++) {
+  splitLetters(textWrapper[i]);
+ }
 
-words[currentWord].style.opacity = 1;
-for (var i = 0; i < words.length; i++) {
-  splitLetters(words[i]);
-}
-
-function changeWord() {
-  var cw = wordArray[currentWord];
-  var nw = currentWord == words.length-1 ? wordArray[0] : wordArray[currentWord+1];
-  for (var i = 0; i < cw.length; i++) {
-    animateLetterOut(cw, i);
-  }
-  
-  for (var i = 0; i < nw.length; i++) {
-    nw[i].className = 'letter behind';
-    nw[0].parentElement.style.opacity = 1;
-    animateLetterIn(nw, i);
-  }
-  
-  currentWord = (currentWord == wordArray.length-1) ? 0 : currentWord+1;
-}
-
-function animateLetterOut(cw, i) {
-  setTimeout(function() {
-		cw[i].className = 'letter out';
-  }, i*80);
-}
-
-function animateLetterIn(nw, i) {
-  setTimeout(function() {
-		nw[i].className = 'letter in';
-  }, 340+(i*80));
-}
-
-function splitLetters(word) {
-  var content = word.innerHTML;
-  word.innerHTML = '';
+ function splitLetters(textWrapper) {
+  var content = textWrapper.innerHTML;
+  textWrapper.innerHTML = '';
   var letters = [];
   for (var i = 0; i < content.length; i++) {
     var letter = document.createElement('span');
     letter.className = 'letter';
     letter.innerHTML = content.charAt(i);
-    word.appendChild(letter);
+    textWrapper.appendChild(letter);
     letters.push(letter);
   }
   
   wordArray.push(letters);
 }
 
-changeWord();
-setInterval(changeWord, 2000);
+anime.timeline({loop: true})
+  .add({
+    targets: '.letter',
+    scale: [0.3,1],
+    opacity: [0,1],
+    translateZ: 0,
+    easing: "easeOutExpo",
+    duration: 600,
+    delay: (el, i) => 70 * (i+1)
+  }).add({
+    targets: '.aboutMeText',
+    opacity: 0,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 1000
+  });
