7062dd16d7ef64fff4b09f951780d46ee497a57b
Natalia328 <nswierczek@gmail.com>
2023-05-08 15:51:58 +0200

    update

--- dev/js/project.js
+++ dev/js/project.js
@@ -57,23 +57,23 @@ window.addEventListener("scroll", contactTitleEffect)
 // Work section on main page
 
 // Wrap every letter in a span
-var textWrapper = document.querySelector('.homePageWorkLink');
-textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+// var textWrapper = document.querySelector('.homePageWorkLink');
+// textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
 
-anime.timeline({loop: true})
-  .add({
-    targets: '.homePageWorkLink .letter',
-    translateX: [40,0],
-    translateZ: 0,
-    opacity: [0,1],
-    easing: "easeOutExpo",
-    duration: 700,
-    delay: (el, i) => 200 + 30 * i
-  }).add({
-    targets: '.homePageWorkLink .letter',
-    translateX: [0,-30],
-    opacity: [1,0],
-    easing: "easeInExpo",
-    duration: 500,
-    delay: (el, i) => 50 + 30 * i
-  });
+// anime.timeline({loop: true})
+//   .add({
+//     targets: '.homePageWorkLink .letter',
+//     translateX: [40,0],
+//     translateZ: 0,
+//     opacity: [0,1],
+//     easing: "easeOutExpo",
+//     duration: 700,
+//     delay: (el, i) => 200 + 30 * i
+//   }).add({
+//     targets: '.homePageWorkLink .letter',
+//     translateX: [0,-30],
+//     opacity: [1,0],
+//     easing: "easeInExpo",
+//     duration: 500,
+//     delay: (el, i) => 50 + 30 * i
+//   });
