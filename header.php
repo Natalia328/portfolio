b3f605cf981d78f4721960dcb1ef98d8f775c0b5
Natalia328 <nswierczek@gmail.com>
2023-05-13 15:59:42 +0200

    update

--- header.php
+++ header.php
@@ -19,6 +19,11 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Aboreto&family=Montserrat+Alternates:wght@100;200;400;500;600;700;800;900&family=Poiret+One&family=Raleway:wght@100;400;700&family=Righteous&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Fahkwang:wght@700&family=Kodchasan:wght@700&family=Krub:wght@600;700&family=Montserrat+Alternates:wght@100;200;300;400;500;600;700;800;900&family=Noto+Serif+Display:wght@700;800;900&display=swap" rel="stylesheet">
     <!-- Link with fonts to be updated, after final decision, which to use -->
 
 
@@ -29,4 +34,3 @@
 <body>
 
     <main>
-    
