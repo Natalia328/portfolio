f5c2b176ee9a8afe32d5678f3f70870db014e59d
Natalia328 <nswierczek@gmail.com>
2023-05-16 19:12:58 +0200

    update

--- dev/js/project.js
+++ dev/js/project.js
@@ -303,6 +303,7 @@ anime.timeline({ loop: true })
 
 // Line moving
 let lines = document.querySelectorAll('.text-1');
+let description = document.querySelector('.aboutMeDescrpition');
 
 window.onscroll = () => {
     let pos = (window.scrollY - 50) / 20;
@@ -313,6 +314,8 @@ window.onscroll = () => {
             lines[i].style.right = `${pos}px`;
         }
     }
+    description.style.right = `${pos}px`;
+
 }
 
 function splitLetters(text) {
