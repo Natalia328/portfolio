494393e690b3be4c5e9caa785cabe0670b1b5e77
Natalia328 <nswierczek@gmail.com>
2023-06-06 15:04:46 +0200

    update

--- template-parts/page-home/page-home-base.php
+++ template-parts/page-home/page-home-base.php
@@ -91,7 +91,7 @@
     </div>
 </section>
 
-<!-- <section id="homePageWork" class="homePage homePageOpening homePageWork">
+<section id="homePageWork" class="homePage homePageOpening homePageWork">
     <h2 class="headerTitle"><?php the_field('homePageWork_mainTitle'); ?></h2>
     <div id="homePageWorkWrapper" class="homePageWorkWrapper">
         <?php $allProjects = get_field('projectPages');
@@ -106,7 +106,7 @@
             </a>
         <?php endforeach ?>
     </div>
-</section> -->
+</section>
 
 <section id="homePageContact" class="homePage homePageContact">
 
